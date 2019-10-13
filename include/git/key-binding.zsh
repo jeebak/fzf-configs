3ae009a9c320f4635894f9819cd070631d4f0134
JeeBak Kim <jeebak.kim@gmail.com>
2019-10-13 12:16:50 -0700

    Organize keybindings

--- include/git/key-binding.zsh
+++ include/git/key-binding.zsh
@@ -47,11 +47,43 @@ bind-git-helper-no-join s
 
 unset -f bind-git-helper bind-git-helper-no-join
 
-bindkey -s '^g^_' " ^ufzf-git\n^y^h"
-bindkey -s '^g^d' " ^ugit diff\n^y^h"
+# Avail.   '^g^q'
+#   "      '^g^w'
 bindkey -s '^g^e' " ^ufzf-git edit-modified\n^y^h"
-bindkey -s '^g^g' " ^ugit status\n^y^h"
+# Remotes  '^g^r'
+# Tags     '^g^t'
+# Avail.   '^g^y'
+#   "      '^g^u'
+#   "      '^g^i'
+#   "      '^g^o'
 bindkey -s '^g^p' " ^ugit pull\n^y^h"
+# Avail.   '^g^['
+#   "      '^g^]'
+# N/A      '^g^\'
+
+# Aliases  '^g^a'
+# Stashes  '^g^s'
+bindkey -s '^g^d' " ^ugit diff\n^y^h"
+# Files    '^g^f'
+bindkey -s '^g^g' " ^ugit status\n^y^h"
+# Hashes   '^g^h'
+# Avail.   '^g^j'
+#   "      '^g^k'
+# Logs     '^g^l'
+# N/A      '^g^;'
+# N/A      '^g^''
+
+# Avail.   '^g^z'
+#   "      '^g^x'
+# N/A      '^g^c'
+# Avail.   '^g^v'
+# Branches '^g^b'
+# Avail.   '^g^n'
+#   "      '^g^m'
+# N/A      '^g^,'
+# N/A      '^g^.'
+bindkey -s '^g^_' " ^ufzf-git\n^y^h"
+
 # NOTE: no "\n^y" like the others; both to allow to add extra params, and as a
 # safguard
 bindkey -s '^g^[p' " ^ugit push ^h"
